531744d8a98a033fdb5a0397f2c32364e3162bdc
lucasmcar <lmc.lucas90@gmail.com>
2025-01-26 22:38:39 -0300

    Adicionados alguns metodos restantes do servico controller e router

--- src/controllers/servico_controller.js
+++ src/controllers/servico_controller.js
@@ -18,31 +18,20 @@ exports.getServicos = async (req, res) => {
         }
 
         // Caso contrário, retorna os resultados encontrados
-        res.status(200).json({ // Inclui a URL base na resposta (opcional)
+        res.status(200).json({
             data: results.rows,
     });
     } catch (err) {
         console.error('Erro na consulta:', err);
         res.status(500).send('Erro interno do servidor');
     }
-   /* const { serviceRoute } = req.params;
-     const query = `SELECT * FROM ${TABLE_NAME} where service_route = $1`;
-     await pool.query(query, [serviceRoute], (err, results) => {
-        if (err) {
-            throw err;
-        }
-        res.statusCode = 200;
-        res.json(results.rows);
-    });*/
+   
 }
 
 exports.addServico = async (req, res) => {
 
-    const { serviceRoute } = req.params; 
-
-    const tableName = routeMappings[serviceRoute];
-    const {descricao, valor, data_servico, id_cliente } = req.body;
-    const query = `INSERT INTO ${tableName} (descricao, valor, data_servico, id_cliente) VALUES ($1, $2, $3, $4)`;
+    const {descricao, valor, data_servico, aprovado, codigo, id_cliente } = req.body;
+    const query = `INSERT INTO ${tableName} (descricao, valor, data_servico, aprovado, codigo, id_cliente) VALUES ($1, $2, $3, $4, $5, $6)`;
     pool.query(query, [descricao, valor, data_servico, id_cliente], (err, results) => {
         if (err) {
             throw err;
@@ -50,6 +39,77 @@ exports.addServico = async (req, res) => {
         res.status(201).send(`Serviço adicionado com sucesso!`);
     });
 }
+
+exports.updateServico = async (req, res) => {
+    const { id } = req.params;
+    const {descricao, valor, data_servico, aprovado, id_cliente } = req.body;
+
+    const query = `UPDATE ${TABLE_NAME} SET descricao = $1, valor = $2, data_servico = $3, aprovado = $4, codigo = $5, id_cliente = $6 WHERE id = $7`;
+    try{
+        pool.query(query, [descricao, valor, data_servico, aprovado, id_cliente, id], (err, results) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).send(`Serviço atualizado com sucesso!`);
+        });
+    } catch( err ){
+        console.error('Erro na consulta:', err);
+        res.status(500).send('Erro interno do servidor');
+    }
+}
+
+exports.servicoPorCodigo = async (req, res) => {
+    const { codigo } = req.params;
+
+    const query = `SELECT * FROM ${TABLE_NAME} WHERE codigo = $1`;
+    try{
+        pool.query(query, [codigo], (err, results) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).json(results.rows);
+        });
+    } catch( err ){
+        console.error('Erro na consulta:', err);
+        res.status(500).send('Erro interno do servidor');
+    }
+}
+
+exports.servicoPorCliente = async (req, res) => {
+    const { id_cliente } = req.params;
+
+    const query = `SELECT * FROM ${TABLE_NAME} WHERE id_cliente = $1`;
+    try{
+        pool.query(query, [id_cliente], (err, results) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).json(results.rows);
+        });
+    } catch( err ){
+        console.error('Erro na consulta:', err);
+        res.status(500).send('Erro interno do servidor');
+    }
+}
+
+exports.deleteServico = async (req, res) => {
+    const { codigo } = req.params;
+
+    const query = `DELETE FROM ${TABLE_NAME} WHERE id = $1`;
+    try{
+        pool.query(query, [codigo], (err, results) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).send(`Serviço deletado com sucesso!`);
+        });
+    } catch( err ){
+        console.error('Erro na consulta:', err);
+        res.status(500).send('Erro interno do servidor');
+    }
+}
+
+
 
 exports.testeTabela = async (req, res) => {
     const query = `
--- src/routes/servico_route.js
+++ src/routes/servico_route.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const servicoController = require('../controllers/servico_controller');
+const express = require("express");
+const servicoController = require("../controllers/servico_controller");
+const { authenticateToken } = require("../middlewares/auth_middleware");
 
 const router = express.Router();
 
-
-
-router
-    .route('/teste')
-    .get(servicoController.testeTabela);
+router.route("/teste").get(servicoController.testeTabela);
 
 router
-    .route('/add/servico')
-    .post(servicoController.addServico);
+  .route("/add/servico")
+  .post(authenticateToken, servicoController.addServico);
 
-    router
-    .route('/servicos')
-    .get(servicoController.getServicos);
+router
+    .route("/servicos")
+    .get(authenticateToken, servicoController.getServicos);
+
+router
+  .route("/edit/servico/:id")
+  .get(authenticateToken, servicoController.updateServico);
+router
+  .route("/servico/:codigo")
+  .get(authenticateToken, servicoController.servicoPorCodigo);
+router
+  .route("/delete/servico/:id_cliente")
+  .get(authenticateToken, servicoController.servicoPorCliente);
 
 module.exports = router;
-
